401d9357492935bb44f17b3cff6438905c029048
Sam Niechcial <samniechcial@gmail.com>
2020-02-25 15:58:16 +0000

    Minor syntax changes to ENV_exec__EnvConsul script

--- ENV_exec_with_EnvConsul_CLI.py
+++ ENV_exec_with_EnvConsul_CLI.py
@@ -1,13 +1,20 @@
 import os
 
 def getFromEnv():
+
+    # Extracts the MYSQL Credential Variables from the EnvConsul Sub-shell:
+
     username = str(os.environ.get("DATABASE_CREDS_LEGACY_MYSQL_ROLE_USERNAME"))
     password = str(os.environ.get("DATABASE_CREDS_LEGACY_MYSQL_ROLE_PASSWORD"))
+
+    # Error handling at it's most rudimentary:
 
     if password == "None":
         print "Script has failed to obtain variables from the environment with EnvConsul"
         print "Please ensure you call this script using the envconsul command via CLI as specified in the readme"
 
+    # Interpolate MySQL credentials from Vault API via EnvConsul and serve to STDOUT:
+    
     else:
         print ""
         print "-----------------------------------------------"
