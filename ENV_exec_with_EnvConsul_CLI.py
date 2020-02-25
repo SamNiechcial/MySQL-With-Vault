e5bae53b1326865a8322150e582d2522ad42c41e
Sam Niechcial <samniechcial@gmail.com>
2020-02-25 15:50:53 +0000

    Add Python script to be called directly from the CLI with the EnvConsul CLI tool

---
+++ ENV_exec_with_EnvConsul_CLI.py
@@ -0,0 +1,23 @@
+import os
+
+def getFromEnv():
+    username = str(os.environ.get("DATABASE_CREDS_LEGACY_MYSQL_ROLE_USERNAME"))
+    password = str(os.environ.get("DATABASE_CREDS_LEGACY_MYSQL_ROLE_PASSWORD"))
+
+    if password == "None":
+        print "Script has failed to obtain variables from the environment with EnvConsul"
+        print "Please ensure you call this script using the envconsul command via CLI as specified in the readme"
+
+    else:
+        print ""
+        print "-----------------------------------------------"
+        print "VARIABLES POPULATED SUCCESSFULLY WITH ENVCONSUL"
+        print "-----------------------------------------------" + "\n"
+        print "Your credentials are:"
+        print "Username = " + username
+        print "Password = " + password                          + "\n"
+        print "These credentials will expire after 1hr"         + "\n"
+        print "-----------------------------------------------"
+        print "-----------------------------------------------" + "\n"
+
+getFromEnv()
